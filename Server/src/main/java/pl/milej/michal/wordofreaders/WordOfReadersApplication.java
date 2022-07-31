ed153a3fc5e8f2115631f9236783a2325f1e71c8
Michał Milej <michal.45454@gmail.com>
2022-07-31 21:35:47 +0200

    Projects separated on Web-browser and server

---
+++ Server/src/main/java/pl/milej/michal/wordofreaders/WordOfReadersApplication.java
@@ -0,0 +1,13 @@
+package pl.milej.michal.wordofreaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordOfReadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordOfReadersApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/pl/milej/michal/wordofreaders/WordOfReadersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milej.michal.wordofreaders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordOfReadersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
