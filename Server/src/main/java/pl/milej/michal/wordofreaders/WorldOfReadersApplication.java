77d575776a70ea64a935cb58f07a97334873d969
Michał Milej <michal.45454@gmail.com>
2023-01-03 18:48:18 +0100

    WordOfReadersApplication renamed to WorldOfReaders...

--- Server/src/main/java/pl/milej/michal/wordofreaders/WorldOfReadersApplication.java
+++ Server/src/main/java/pl/milej/michal/wordofreaders/WorldOfReadersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WordOfReadersApplication {
+public class WorldOfReadersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WordOfReadersApplication.class, args);
+		SpringApplication.run(WorldOfReadersApplication.class, args);
 	}
 
 }
